c1a4efa2615f51bf574dbe27f21dfedb9881cd96
Alexey Lyutikh <lutixalex1998@gmail.com>
2021-02-10 21:01:13 +0300

    add design

--- main.go
+++ main.go
@@ -1,38 +1,49 @@
 package main
 
-import ("fmt"
-        "net/http")
+import ("fmt"; "net/http"; "html/template")
 // импортируем дополнительные пакеты
 
 type User struct {
-  name string
-  age uint16
-  money int16
-  avg_grades, happiness float64
+  Name string
+  Age uint16
+  Money int16
+  Avg_grades, Happiness float64
+  Hobbies []string
 }
 
+
+
 func (u User) getAllInfo() string {
-  return fmt.Sprintf("username is: %s. He is %d and he has money: %d", u.name, u.age, u.money)
+  return fmt.Sprintf("username is: %s. He is %d and he has money: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) SetNewName(newName string) {
   // чтобы изменить саму струтуру НУЖНА ссылка
-  u.name = newName
+  u.Name = newName
 }
 
-func home_page(w http.ResponseWriter, r *http.Request){
+func home_page2(w http.ResponseWriter, r *http.Request){
   // Request - запрос, который всегда передается
   // -------------------------------------------- //
   fmt.Fprintf(w, "hello Go Го\n")
   // задаем форматированную строку
   // w - куда записываем
   // -------------------------------------------- //
-  bob := User{"Bob", 25, -50, 4.2, 0.8}
+  bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"fotbool", "Skate", "dance"}}
   // bob.name = "Alex" - set new name
 
   bob.SetNewName("Alex")
   fmt.Fprintf(w, bob.getAllInfo())
+
 }
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+  // fmt.Fprintf(w, "<b>Main Text</b>")
+  bob := User{"Bob", 25, -50, 4.2, 0.8, []string{"fotbool", "Skate", "dance"}}
+  teml, _ := template.ParseFiles("templates/home_page.html")
+  teml.Execute(w, bob)
+}
+
 
 func contacts_page(w http.ResponseWriter, r *http.Request){
   fmt.Fprintf(w, "contact page")
